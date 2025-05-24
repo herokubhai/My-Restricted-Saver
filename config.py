a0212e35118f5eb492ae323f49b0a0fd13a75c9f
herokubhai <149265017+herokubhai@users.noreply.github.com>
2025-05-24 18:38:54 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7575487741:AAGVL1WMiHr5j88NIPx2EQD4CBsT
 API_ID = int(os.environ.get("API_ID", "28203009")) # Defaulting to "0" as int for safety if not set
 API_HASH = os.environ.get("API_HASH", "c385478ec9a0c322964bdd56175cef7b")
 ADMINS = int(os.environ.get("ADMINS", "6401029823")) # Defaulting to "0" as int for safety
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://modkha7110:modkha7110@cluster0.4jiwjny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002547260781") # Will be an empty string if not set
